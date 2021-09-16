76164cd17efda3a92a5518a1015b47d0a2393f95
ahra1221 <lizze1221@sookmyung.ac.kr>
2021-09-16 23:49:11 +0900

    lizze1221
    성공

--- WEEK2-A/lizze1221/1038.cpp
+++ WEEK2-A/lizze1221/1038.cpp
@@ -12,16 +12,29 @@ int main(){
 
     cin >> n;
 
-    for(int i=1;i<10;i++) arr[i] = i;
-
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<(arr[i]%10);j++){
-            arr[++tmp] = arr[i]*10 + j;
-        }
+    queue <long long> q;
+    for(int i=1;i<10;i++) {
+        arr[i] = i;
+        q.push(i);
     }
 
-    if(arr[n]!=0 && tmp >= n) cout << arr[n]<<"\n";
-    else cout <<"-1\n";
+     if (0 <= n && n <= 10) {
+        cout << n << endl;
+        return 0;
+    }
+
+    while (tmp <= n && !q.empty()){
+        long long num = q.front();
+        q.pop();
+ 
+        for (int i = 0; i < num % 10; i++){
+            q.push(num * 10 + i);
+            arr[++tmp] = num * 10 + i;
+        }
+    }
+ 
+    if (tmp >= n && arr[n] != 0) cout << arr[n] << "\n";
+    else cout << "-1\n";
 
     return 0;
 }
