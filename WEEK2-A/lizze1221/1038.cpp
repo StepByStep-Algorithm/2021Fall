2940136eb04dfe5cebf6d1732114dca161f72524
ahra1221 <lizze1221@sookmyung.ac.kr>
2021-09-16 12:05:24 +0900

    lizze1221
    runtime error...

--- WEEK2-A/lizze1221/1038.cpp
+++ WEEK2-A/lizze1221/1038.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int n;
 queue <long long> q;
+long long arr[1000001];
 
 int main(){
     ios::sync_with_stdio(false);
@@ -11,19 +12,26 @@ int main(){
 
     cin >> n;
 
-    if(n >0 && n < 10) cout << n;
+    for(int i=1;i<10;i++) {
+        q.push(i);
+        arr[i] = i;
+    }
+
+    if(n >=0 && n < 10) cout << arr[n];
     else if (n < 0) cout <<"-1";
     else{
-       int tmp = 0;
+       int tmp = 9;
 
-       for(int i=1;i<10;i++) q.push(i);
-       while(++tmp < n){
+       while(tmp <= n){
            long long num = q.front();
            q.pop();
 
-           for(int i=0;i<num;i++) q.push(num*10 + i);
+           for(int i=0;i<num;i++){
+               q.push(num*10 + i);
+               arr[++tmp] = num*10 + i;
+           }
        }
-       cout << q.front()<<"\n";
+       cout << arr[n] <<"\n";
     }
     return 0;
 }
