1d47e748b1d702506d48dcfd2a5de200b7acaa26
EUNA-319 <81363864+EUNA-319@users.noreply.github.com>
2021-09-17 13:49:39 +0900

    EUNA-319 boj2693

--- WEEK2-B/EUNA-319/boj2693.cpp
+++ WEEK2-B/EUNA-319/boj2693.cpp
@@ -2,43 +2,13 @@
 #include <algorithm>
 using namespace std;
 
-class List{
-public:
-    int age;
-    string name;
-};
-
-void sorting(List *list);
-
 int main() {
+    int a[10];
     int tc;
     cin>>tc;
-    List *list=new List[tc];
-
-    for(int i=0; i<tc;i++){
-        cin>>list[i].age>>list[i].name;
-    }
-    sorting(list,list+tc);
     for(int i=0;i<tc;i++){
-        cout<<list[i].age<<" "<<list[i].name<<"\n";
-    }
-    delete [] list;
-
-}
-
-void sorting(List *list){
-    int temp1; // 수를 바꿀 때 사용
-    string temp2;
-    for(int i=0;i<2;i++){ // 앞에 있는 수가 뒤에 있는 수 보다 작다면 계속해서 뒤로 밀림
-        for(int j=i+1;j<3;j++){ // i보다 큰 숫자들에 대해 비교 반복
-            if(list[i].age>list[j].age){
-                temp1=list[i].age;
-                temp2=list[i].name;
-                list[i].age=list[j].age;
-                list[i].name=list[j].name;
-                list[j].age=temp1;
-                list[j].name=temp2;
-            }
-        }
+        scanf("%d %d %d %d %d %d %d %d %d %d",&a[0],&a[1],&a[2],&a[3],&a[4],&a[5],&a[6],&a[7],&a[8],&a[9]);
+        sort(a,a+10);
+        cout<<a[7]<<"\n";
     }
 }
