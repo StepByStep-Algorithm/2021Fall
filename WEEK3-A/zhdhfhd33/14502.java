f93ea97aff5e47de77528a4ba9091f03b64c28bf
zhdhfhd33 <43158514+zhdhfhd33@users.noreply.github.com>
2021-09-23 01:57:17 +0900

    zhdhfhd33 boj14502

---
+++ WEEK3-A/zhdhfhd33/14502.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+class Pair{
+	int x;
+	int y;
+	Pair(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		int N, M;
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextInt();
+		M=sc.nextInt();
+		int arr[][]=new int[N][M];
+		
+		
+	}
+
+}
