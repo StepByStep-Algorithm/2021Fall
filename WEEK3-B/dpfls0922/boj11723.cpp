96544374276d3999738376cb460b6c4365c519e5
dpfls0922 <83818069+dpfls0922@users.noreply.github.com>
2021-09-26 10:05:46 +0900

    dpfls0922 boj11723

---
+++ WEEK3-B/dpfls0922/boj11723.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main(){
+    int tc, x;
+    string func;
+    int alp[21] = {0, };
+    string function[6] = {"add", "remove", "check", "toggle", "all", "empty"};
+    cin >> tc;
+    for(int i=0; i<tc; i++){
+        cin >> func;
+        for(int j=0; j<6; j++){
+            int idx = (func == function[i]);
+            switch(idx){
+                case 1:  // add
+                cin >> x;
+                alp[x] = 1;
+                break;
+                // x ÀÖÀ¸¸é continue
+                case 2:  // remove
+                cin >> x;
+                alp[x] = 0;
+                break;////
+                case 3: // check
+                cin >> x;
+                int state = alp[x]==1 ? 1 : 0;
+                cout << state << '\n';
+                case 4: // toggle
+                cin >> x;
+                alp[x]==1 ? alp[x]=0 : alp[x]=1;
+                case 5: // all
+                for(int i=0; i<21; i++)
+                    alp[i]=1;
+                case 6: //empty
+                for(int i=0; i<21; i++)
+                    alp[i]=1;
+            }
+        }
+    }
+    return 0;
+}
