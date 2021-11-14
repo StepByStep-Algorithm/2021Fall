04929842070de831b9629ec22abdbd675ff01e1c
nawon <nwkim@dgu.ac.kr>
2021-11-14 09:37:58 +0900

    naw0n boj9372

--- WEEK5-B/naw0n/boj9372.cpp
+++ WEEK5-B/naw0n/boj9372.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
+#include <queue>
+
 using namespace std;
 
-int main() {
+bool line[1001][1001] = { 0, };
+bool bfs_check[1001] = { 0, };
+bool dfs_check[1001] = { 0, };
 
-	int T; // 테스트 케이스 수
-	cin >> T;
+void dfs(int n, int v) {
+	dfs_check[v] = 1;
+	cout << v << " ";
 
-	while (T--) {
-		int n, m; // 국가 수, 비행기 종류
-		cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+		if (line[v][i] && !dfs_check[i])
+			dfs(n, i);
+}
 
-		int a, b;
-		while (m--) {
-			cin >> a >> b;
-		}
+void bfs(int n, int v) {
+	queue<int> q;
 
-		cout << n - 1 << endl;
+	bfs_check[v] = 1;
+	q.push(v);
+	while (!q.empty()) {
+		int now = q.front();
+		cout << now << " ";
+		q.pop();
+		for (int i = 1; i <= n; i++)
+			if (line[now][i] && !bfs_check[i]) {
+				q.push(i);
+				bfs_check[i] = 1;
+			}
 	}
+}
+
+int main(void) {
+	int n, m, v;
+
+	cin >> n >> m >> v;
+	while (m--) {
+		int x, y;
+		cin >> x >> y;
+		line[x][y] = 1;
+		line[y][x] = 1;
+	}
+	dfs(n, v);
+	cout << "\n";
+	bfs(n, v);
+	return 0;
 }
